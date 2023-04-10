060abda1d79db8e015a5d410bc692aa82a2f2f8d
Miika Rinta-Korkeamäki <e1900516@edu.vamk.fi>
2023-04-10 12:04:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,28 @@ import './App.css';
 
 function App() {
   
-  const [number1, setNumber1] = useState();
-  const [number2, setNumber2] = useState();
+  const [numbers, setNumbers] = useState({number1: '', number2: ''});
   const [result, setResult] = useState(0);
  
   const inputChanged = (event) => {
-    setNumber1({...number1, [event.target.name]: event.target.value});
-    setNumber2({...number2,[event.target.name]: event.target.value});
-    setResult({...result, [event.target.name]: event.target.value});
+    setNumbers({...numbers, [event.target.name]: event.target.value});
   }
   
   const inputSum = () => {
     
-    setResult (Number(number1) + Number(number2));
+    setResult(Number(numbers.number1) + Number(numbers.number2));
   }
 
   const inputSub = () => {
     
-    setResult(Number(number1) - Number(number2));
+    setResult(Number(numbers.number1) - Number(numbers.number2));
   }
 
   return (
     <div className="App">
       Result = {result}<br />
-      <input name="number1" value={number1} onChange={inputChanged}/>
-      <input name="number2" value={number2} onChange={inputChanged}/>
+      <input name="number1" value={numbers.number1} onChange={inputChanged}/>
+      <input name="number2" value={numbers.number2} onChange={inputChanged}/>
       <button onClick={inputSum}>+</button>
       <button onClick={inputSub}>-</button>
     </div>
